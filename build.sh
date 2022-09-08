d9b36cb2deddad44c7fb4f391b9d2348305e69d9
Qilin <qilin.cheng@foxmail.com>
2022-09-09 01:58:05 +0800

    cmake build system

---
+++ build.sh
@@ -0,0 +1,20 @@
+
+rm -rf ./cmake-build-debug
+mkdir cmake-build-debug
+cd cmake-build-debug
+cmake -DCMAKE_BUILD_TYPE=Debug ..
+cmake --build . --config Debug
+cd ..
+
+rm -rf ./cmake-build-release
+mkdir cmake-build-release
+cd cmake-build-release
+cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake --build . --config Release
+cd ..
+
+rm -rf ./cmake-install
+cmake --install cmake-build-debug --prefix ./cmake-install --config Debug
+cmake --install cmake-build-release --prefix ./cmake-install --config Release
+
+pause
